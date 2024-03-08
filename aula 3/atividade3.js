8494914b8dea06aad1343e9213b2f6ec9fcc00e1
Amanda06ldo <162368771+Amanda06ldo@users.noreply.github.com>
2024-03-08 17:13:01 -0300

    Add files via upload

---
+++ aula 3/atividade3.js
@@ -0,0 +1,3 @@
+const numero1 = '14'
+const numero2 = '11'
+console.log(Number(numero1)+Number(numero2))
---
+++ aula 3/aula03.js
@@ -0,0 +1,9 @@
+//Este código imprime uma mensagem
+const nome = 'Amanda'
+let idade = 17
+const altura = 1.60
+const temperatura = -20
+let souUmBoolean = true
+souUmBoolean = false
+console.log('Meu nome é',nome,'e tenho',idade,'anos.')
+console.log('Olá mundo!')
