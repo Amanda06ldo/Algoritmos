a3e69c77eba00233cc94347f5fd9f58396f4e2ee
Amanda06ldo <162368771+Amanda06ldo@users.noreply.github.com>
2024-03-13 13:52:31 -0300

    Add files via upload

---
+++ Interpretação/retangulo.js
@@ -0,0 +1,4 @@
+let base = 2
+let altura = 5
+const multiplicacao = 2 * 5
+console.log(multiplicacao)
---
+++ Interpretação/strings.js
@@ -0,0 +1,8 @@
+const nome = 'Amanda'
+const sobrenome = 'lima de oliveira'
+const nomeCompleto = 'Amanda lima de oliveira'
+console.log(nomeCompleto)
+let valor1 = 50
+let valor2 = 25
+const soma = valor1 + valor2
+console.log(soma)
