b742c87057cb79483c88b7f83cbf70fe651e8c67
Amanda06ldo <162368771+Amanda06ldo@users.noreply.github.com>
2024-03-13 14:34:56 -0300

    Add files via upload

--- Interpretação/nota.js
+++ Interpretação/nota.js
@@ -1,9 +1,5 @@
 let nota1 = 20
 let nota2 = 25
 let nota3 = 30
-const soma = nota1 + nota2 + nota3
+const soma = (nota1 + nota2 + nota3) / 3
 console.log(soma)
-let nota4 = 75
-let media = 3
-const divisao = nota1 + nota2 / nota3
-console.log(divisao)
--- Interpretação/strings.js
+++ Interpretação/strings.js
@@ -1,8 +1,9 @@
 const nome = 'Amanda'
-const sobrenome = 'lima de oliveira'
-const nomeCompleto = 'Amanda lima de oliveira'
+const sobrenome = ' lima de oliveira'
+const nomeCompleto = nome + sobrenome
 console.log(nomeCompleto)
 let valor1 = 50
 let valor2 = 25
 const soma = valor1 + valor2
 console.log(soma)
+console.log(Number(valor1) + Number(valor2))     
