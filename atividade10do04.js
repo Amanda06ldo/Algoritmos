9fbf2659dce028e3bfe7a15c36fb1948aeae2001
Amanda06ldo <162368771+Amanda06ldo@users.noreply.github.com>
2024-04-10 16:11:41 -0300

    Add files via upload

---
+++ atividade10do04.js
@@ -0,0 +1,18 @@
+
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+for(let i = 0; i < numeros.length; i++){
+    const elemento=numeros[i]
+    console.log(elemento)
+}
+
+for(numerosPares = 0; numerosPares <= 10; numerosPares += 2){
+    console.log(numerosPares)
+}
+
+for(tabuada = 0; tabuada <= 50;tabuada++){
+    console.log(tabuada * 5)
+    }
+    
+   
+for(soma = 1; soma <= 10; soma++){
+    console.log(soma + soma)}
